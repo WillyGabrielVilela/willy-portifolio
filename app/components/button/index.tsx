314bfbeea555543dc1fbb78aac945a003f4c7cf8
Willy Gabriel Vilela <willygabrielnoval@gmail.com>
2023-08-15 20:49:27 -0300

    Completo

---
+++ app/components/button/index.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/app/lib/utils";
+import { ButtonHTMLAttributes } from "react";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button = ({ children, className, ...props }: ButtonProps) => {
+    return (
+        <button
+        className={cn(
+            "bg-blue-600 py-3 px-4 rounded-lg text-gray-50 flex items-center justify-center gap-2 hover:bg-blue-500 transition-all disabled:opacity-50"
+        )}
+        {...props}
+        >
+            {children}
+        </button>
+    )
+}
